0d30ecede22c787f989e7b9103d197929b777775
Yousef Fatouraee <fatouraee96@gmail.com>
2022-02-02 00:06:07 +0330

    added anime.js as animation library - SVGs are now set - car moves

--- majhoolsoft-react/src/App.js
+++ majhoolsoft-react/src/App.js
@@ -1,20 +1,39 @@
 import './App.css';
-import Car from './components/Car.js'
+import { useEffect } from 'react';
+import Car from './components/Car';
+import Background from './components/Background';
+import anime from 'animejs';
+
+
 function App() {
+
+  let generalView = "0 0 1920 1080";
+  let beginView = "400 0 200 200";
+  let doorView = "1002.43 900.24 384.26 136.1";
+  let carView = "0 650 900 400";
+
+  // useEffect(() => {
+  //   anime({
+  //     targets:".page",
+  //     duration: 20000,
+  //     viewBox: ["0 0 1920 1080",carView]
+  //   });
+  // }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          <Car />
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <svg 
+            xmlns="http://www.w3.org/2000/svg"
+             className="page" 
+             width={1920} 
+             height={1080}>
+
+          <Background className="background">
+          </Background>
+          <Car/>
+        </svg>
+
       </header>
     </div>
   );
