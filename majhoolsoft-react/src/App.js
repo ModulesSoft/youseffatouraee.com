aeb4ed6de2c1ba50523fd102ff9a764467fd903d
Yousef <yousef.fatouraa@aut.ac.ir>
2022-02-02 15:26:48 +0330

    the car moves to backyard with camera

--- majhoolsoft-react/src/App.js
+++ majhoolsoft-react/src/App.js
@@ -1,39 +1,31 @@
-import './App.css';
-import { useEffect } from 'react';
-import Car from './components/Car';
-import Background from './components/Background';
-import anime from 'animejs';
-
+import "./App.css";
+import { useEffect } from "react";
+import Car from "./components/Car";
+import Background from "./components/Background";
+import anime from "animejs";
 
 function App() {
-
   let generalView = "0 0 1920 1080";
   let beginView = "400 0 200 200";
   let doorView = "1002.43 900.24 384.26 136.1";
   let carView = "0 650 900 400";
 
-  // useEffect(() => {
-  //   anime({
-  //     targets:".page",
-  //     duration: 20000,
-  //     viewBox: ["0 0 1920 1080",carView]
-  //   });
-  // }, []);
-  
+  useEffect(() => {
+    anime({
+      targets: "#page",
+      duration: 3000,
+      viewBox: ["0 650 900 500", "1000 0 1940 1080"],
+      easing: "easeInOutQuart",
+      delay: 500,
+    });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <svg 
-            xmlns="http://www.w3.org/2000/svg"
-             className="page" 
-             width={1920} 
-             height={1080}>
-
-          <Background className="background">
-          </Background>
-          <Car/>
+        <svg className="page" id="page">
+          <Background />
+          <Car />
         </svg>
-
       </header>
     </div>
   );
