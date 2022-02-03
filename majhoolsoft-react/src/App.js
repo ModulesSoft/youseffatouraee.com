d90415027cb530cdd4b8cadc9f70f85aa2703de8
Yousef Fatouraee <fatouraee96@gmail.com>
2022-02-03 11:17:29 +0330

    light beam and darkness added

--- majhoolsoft-react/src/App.js
+++ majhoolsoft-react/src/App.js
@@ -3,20 +3,44 @@ import { useEffect } from "react";
 import Car from "./components/Car";
 import Background from "./components/Background";
 import anime from "animejs";
-
+import Darkness from "./components/Darkness";
+import LightBeam from "./components/LightBeam";
 function App() {
   let generalView = "0 0 1920 1080";
   let beginView = "400 0 200 200";
   let doorView = "1002.43 900.24 384.26 136.1";
   let carView = "0 650 900 400";
-
+  // camera transition from the car to the background
   useEffect(() => {
     anime({
       targets: "#page",
-      duration: 3000,
-      viewBox: ["0 650 900 500", "1000 0 1940 1080"],
-      easing: "easeInOutQuart",
-      delay: 500,
+      duration: 4000,
+      viewBox: ["0 650 900 500", "1000 0 2240 1080"],
+      easing: "easeOutQuad",
+      delay: 5000,
+      // delay: 500,
+    });
+  }, []);
+  // fade darkness to brightness
+  useEffect(() => {
+    anime({
+      targets: "#darkness",
+      duration: 10000,
+      translateY: "5000px",
+      easing: "easeOutQuad",
+      delay: 9000,
+      // delay: 500,
+    });
+  }, []);
+  // turn off the head lights
+  useEffect(() => {
+    anime({
+      targets: "#beam",
+      duration: 500,
+      opacity: 0,
+      easing: "easeOutQuad",
+      delay: 10000,
+      // delay: 500,
     });
   }, []);
   return (
@@ -25,6 +49,8 @@ function App() {
         <svg className="page" id="page">
           <Background />
           <Car />
+          <Darkness />
+          <LightBeam />
         </svg>
       </header>
     </div>
