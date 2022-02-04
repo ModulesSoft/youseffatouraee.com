fb2580dc96efcc57d1cfa6e37c3193aab6ae4da0
Yousef Fatouraee <fatouraee96@gmail.com>
2022-02-04 13:07:37 +0330

    text added and some of the bugs fixed

--- majhoolsoft-react/src/components/Darkness.js
+++ majhoolsoft-react/src/components/Darkness.js
@@ -3,15 +3,15 @@ function Darkness() {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="2920"
-      height="1080"
-      viewBox="0 0 2920 1080"
+      height="1580"
       x="-1000"
+      y="-100"
     >
       <rect
         id="darkness"
         data-name="Rectangle 148"
         width="2920"
-        height="1080"
+        height="1580"
         fill="rgba(0,0,0,0.77)"
       />
     </svg>
