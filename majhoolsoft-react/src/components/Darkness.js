5e509f4e5a2d4389f80108384e7ecc624d2f1d23
Yousef <yousef.fatouraa@aut.ac.ir>
2022-02-04 23:47:51 +0330

    responsive bugs fix

--- majhoolsoft-react/src/components/Darkness.js
+++ majhoolsoft-react/src/components/Darkness.js
@@ -2,17 +2,17 @@ function Darkness() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="2920"
-      height="1580"
+      width="10000"
+      height="10000"
       x="-1000"
       y="-100"
     >
       <rect
         id="darkness"
         data-name="Rectangle 148"
-        width="2920"
-        height="1580"
-        fill="rgba(0,0,0,0.77)"
+        width="10000"
+        height="10000"
+        fill="rgba(0,0,0,0.9)"
       />
     </svg>
   );
