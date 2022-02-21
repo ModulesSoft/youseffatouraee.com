39f6e97ce1fe1f751790c6bfc98764d206ce35fd
Yousef <yousef.fatouraa@aut.ac.ir>
2022-02-21 20:18:47 +0330

    intro animations clearnace

--- majhoolsoft-react/src/App.js
+++ majhoolsoft-react/src/App.js
@@ -30,6 +30,7 @@ function App() {
   const doorViewDesktop = "1000 800 400 240";
   const doorViewMobile = `1000 ${1080 - height} ${width} ${height}`;
   const laptopView = "1328 920 60 60";
+  const CertificateView = "1288 900 25 25";
 
   let timeline = 0;
 
@@ -43,69 +44,76 @@ function App() {
   }
 
   useEffect(() => {
-    // Scroll icon
-    anime({
-      targets: ".scrollIcon",
-      translateY: -50,
-      direction: "alternate",
-      loop: true,
-      easing: "spring(1, 80, 10, 0)",
-      opacity: 0.3,
-    });
-    // Hi text fades in and out
-    anime({
-      targets: ".hi",
-      opacity: [
-        { value: 1, duration: 1000 },
-        { value: 0, delay: 6000 },
-      ],
-      easing: "easeInExpo",
-      delay: 0,
-    });
-    // welcome text fades in and out
-    anime({
-      targets: ".welcome",
-      opacity: [
-        { value: 1, duration: 1000 },
-        { value: 0, delay: 6000 },
-      ],
-      easing: "easeInExpo",
-      delay: 1000,
-    });
-    // camera transition
-    anime({
-      targets: ".page",
-      duration: 4000,
-      keyframes: [
-        // camera transition from the car in the way home to the background
-        { viewBox: [beginView, generalView], delay: 5000 },
-        // camera transition to garage door
+    //thread one (intro text, camera transition and scroll text):
+    anime
+      .timeline({ loop: false })
+      // Hi text fades in
+      .add({
+        targets: ".hi",
+        opacity: [0, 1],
+        easing: "easeInExpo",
+      })
+      // welcome text fades in
+      .add({
+        targets: ".welcome",
+        opacity: [0, 1],
+        easing: "easeInExpo",
+      })
+      // Hi and welcome text fade out
+      .add(
         {
-          viewBox: [generalView, mobile ? doorViewMobile : doorViewDesktop],
-          delay: 5000,
+          targets: [".hi", ".welcome"],
+          opacity: 0,
+          easing: "easeInExpo",
         },
-      ],
-      easing: "easeOutQuad",
-      delay: 5000,
-    });
+        5000
+      )
+      // camera transition
+      .add({
+        targets: ".page",
+        duration: 4000,
+        keyframes: [
+          // camera transition from the car in the way home to the background
+          { viewBox: [beginView, generalView] },
+          // camera transition to garage door
+          {
+            viewBox: [generalView, mobile ? doorViewMobile : doorViewDesktop],
+            delay: 5000,
+          },
+        ],
+        easing: "easeOutQuad",
+      })
+      // scroll text fades in
+      .add({
+        targets: ".scroll",
+        opacity: 0.8,
+        // duration: 2000,
+        easing: "linear",
+      });
+
+    //thread two (sun and cloud):
     // sun sets
-    anime({
-      targets: "#sun",
-      duration: 3000,
-      translateY: "-650px",
-      easing: "easeOutQuad",
-      delay: 6000,
-    });
-    // clouds come up
-    anime({
-      targets: "#clouds",
-      duration: 1000,
-      translateY: "-300px",
-      // easing: "easeOutQuad",
-      delay: 9000,
-    });
+    anime
+      .timeline({ loop: false })
+      .add(
+        {
+          targets: "#sun",
+          duration: 3000,
+          translateY: "-650px",
+          easing: "easeOutQuad",
+        },
+        6000 //delay to start
+      )
+      // clouds come up
+      .add({
+        targets: "#clouds",
+        duration: 1000,
+        translateY: "-300px",
+      });
+
+    //thread three (darkness):
     // fade away the darkness to brightness and vice versa
-    anime({
+    anime.timeline({ loop: false }).add({
       targets: "#darkness",
       keyframes: [
         // fade in to day
@@ -119,75 +127,78 @@ function App() {
         },
       ],
     });
-    // debug for top gap
-    anime({
-      targets: ".container",
-      duration: 3000,
-      backgroundColor: "#87ceeb",
-      easing: "linear",
-      delay: 7000,
-    });
-    // background color fades to blue
-    anime({
-      targets: "container",
-      backgroundImage: "linear-gradient (#87CEEB, #034b04)",
-      duration: 5000,
-      opacity: 1,
-      delay: 8000,
-    });
+
+    //thread four (headlights):
     // turn off the head lights
     anime({
       targets: "#beam",
       duration: 500,
       opacity: 0,
       easing: "easeOutQuad",
-      delay: 8000,
+      delay: 10000,
     });
-    // scroll text fades in
+
+    //last thread - infinite loop (scroll icon):
+    // Scroll icon
     anime({
-      targets: ".scroll",
-      opacity: 0.8,
-      delay: 15000,
-      duration: 2000,
-      easing: "linear",
+      targets: ".scrollIcon",
+      translateY: -50,
+      direction: "alternate",
+      loop: true,
+      easing: "spring(1, 80, 10, 0)",
+      opacity: 0.3,
     });
+
+    // debug for top gap
+    // anime({
+    //   targets: ".container",
+    //   duration: 3000,
+    //   backgroundColor: "#87ceeb",
+    //   easing: "linear",
+    //   delay: 7000,
+    // });
+    // background color fades to blue
+    // anime({
+    //   targets: ".container",
+    //   backgroundImage: "linear-gradient (#87CEEB, #034b04)",
+    //   duration: 5000,
+    //   opacity: 1,
+    //   delay: 8000,
+    // });
   }, []);
 
   function beginResume() {
-    anime({
-      targets: ".page",
-      duration: 4000,
-      keyframes: [
-        // camera transition from the garage door to the laptop
-        {
-          viewBox: [mobile ? doorViewMobile : doorViewDesktop, laptopView],
-          // viewBox: mobile ? doorViewMobile : doorViewDesktop,
-        },
-        // camera transition to garage door
-        {},
-      ],
-      easing: "easeOutQuad",
-    });
-
-    timeline += 4000;
-
-    //resumeOne text background appearance
-    anime({
-      targets: "#resumeOne",
-      duration: 1000,
-      opacity: [0, 1],
-      easing: "easeInExpo",
-      delay: 2000,
-    });
-    //resumeTwo text background appearance
-    anime({
-      targets: "#resumeTwo",
-      duration: 1000,
-      opacity: [0, 1],
-      easing: "easeInExpo",
-      delay: 8000,
-    });
-    TextAnimation("#resumeOne", 4_000, 100, 10000);
+    anime
+      .timeline({ loop: false })
+      .add({
+        targets: ".page",
+        duration: 4000,
+        keyframes: [
+          // camera transition from the garage door to the laptop
+          {
+            viewBox: [mobile ? doorViewMobile : doorViewDesktop, laptopView],
+            // viewBox: mobile ? doorViewMobile : doorViewDesktop,
+          },
+          // camera transition to garage door
+          {},
+        ],
+        easing: "easeOutQuad",
+      })
+      //resumeOne text background appearance
+      .add({
+        targets: "#resumeOne",
+        duration: 1000,
+        opacity: [0, 1],
+        easing: "easeInExpo",
+      })
+      //resumeTwo text background appearance
+      .add({
+        targets: "#resumeTwo",
+        duration: 1000,
+        opacity: [0, 1],
+        easing: "easeInExpo",
+      });
+    TextAnimation("#resumeOne", 5_000, 100, 10000);
     TextAnimation("#resumeTwo", 10_000, 100, 16000);
     timeline += 4000;
   }
@@ -215,10 +226,10 @@ function App() {
             <rect width="35" height="65"></rect>
           </svg> */}
         </svg>
-        <article>
-          <p className="hi text">Hi</p>
-          <p className="welcome text">Ride with me to my world!</p>
-          <p className="scroll text">Scroll Down</p>
+        <article className="article">
+          <p className="hi">Hi</p>
+          <p className="welcome">Ride with me to my world!</p>
+          <p className="scroll">Scroll Down</p>
           <p id="resumeOne" className="text-animation">
             I'm a software engineer
           </p>
