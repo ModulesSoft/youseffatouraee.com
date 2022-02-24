02c99dd5cf6b18ff230a431f021e7d1546f71e96
Yousef Fatouraee <fatouraee96@gmail.com>
2022-02-24 14:12:17 +0330

    animation added

--- majhoolsoft-react/src/Play.js
+++ majhoolsoft-react/src/Play.js
@@ -16,56 +16,72 @@ function Play(isMobile, width, height, timeline = 0) {
 
   function GarageScene() {
     // start sequence
-    Camera.LaptopView(laptopOne);
-    function laptopOne() {
-      TypewriterAnimation("#laptopOne", 2_000, 100, 1000, laptopTwo);
+    // Camera.LaptopView(laptopOne);
+    // function laptopOne() {
+    //   TypewriterAnimation("#laptopOne", 2_000, 100, 1000, laptopTwo);
+    // }
+    // function laptopTwo() {
+    //   TypewriterAnimation("#laptopTwo", 500, 100, 1000, degreeCamera);
+    // }
+    // function degreeCamera() {
+    //   Camera.CertificateView(degreeOne);
+    // }
+    // function degreeOne() {
+    //   TypewriterAnimation("#degreeOne", 2_000, 100, 1000, degreeTwo);
+    // }
+    // function degreeTwo() {
+    //   TypewriterAnimation("#degreeTwo", 2_000, 100, 1000, OSCamera);
+    // }
+    // function OSCamera() {
+    //   Camera.OSView(OSOne);
+    // }
+    // function OSOne() {
+    //   TypewriterAnimation("#OSOne", 2_000, 100, 1000, frontEndOneCamera);
+    // }
+    // function frontEndOneCamera() {
+    //   Camera.frontEndOneView(frontEndOneOne);
+    // }
+    // // front end
+    // function frontEndOneOne() {
+    //   TypewriterAnimation("#frontEndOneOne", 2_000, 100, 1000, frontEndOneTwo);
+    // }
+    // function frontEndOneTwo() {
+    //   TypewriterAnimation(
+    //     "#frontEndOneTwo",
+    //     2_000,
+    //     100,
+    //     1000,
+    //     frontEndTwoCamera
+    //   );
+    // }
+    // function frontEndTwoCamera() {
+    //   Camera.frontEndTwoView(frontEndTwoOne);
+    // }
+    // function frontEndTwoOne() {
+    //   TypewriterAnimation("#frontEndTwoOne", 2_000, 100, 1000, frontEndTwoTwo);
+    // }
+    // function frontEndTwoTwo() {
+    //   TypewriterAnimation(
+    //     "#frontEndTwoTwo",
+    //     2_000,
+    //     100,
+    //     1000,
+    //     backEndOneCamera
+    //   );
+    // }
+    // back end
+    backEndOneCamera();
+    function backEndOneCamera() {
+      Camera.backEndOneView(backEndOneOne);
     }
-    function laptopTwo() {
-      TypewriterAnimation("#laptopTwo", 500, 100, 1000, degreeCamera);
+    function backEndOneOne() {
+      TypewriterAnimation("#backEndOneOne", 2_000, 100, 1000, backEndOneTwo);
     }
-    function degreeCamera() {
-      Camera.CertificateView(degreeOne);
+    function backEndOneTwo() {
+      TypewriterAnimation("#backEndOneTwo", 2_000, 100, 1000, test);
     }
-    function degreeOne() {
-      TypewriterAnimation("#degreeOne", 2_000, 100, 1000, degreeTwo);
-    }
-    function degreeTwo() {
-      TypewriterAnimation("#degreeTwo", 2_000, 100, 1000, OSCamera);
-    }
-    function OSCamera() {
-      Camera.OSView(OSOne);
-    }
-    function OSOne() {
-      TypewriterAnimation("#OSOne", 2_000, 100, 1000, frontEndOneCamera);
-    }
-    function frontEndOneCamera() {
-      Camera.frontEndOneView(frontEndOneOne);
-    }
-    function frontEndOneOne() {
-      TypewriterAnimation("#frontEndOneOne", 2_000, 100, 1000, frontEndOneTwo);
-    }
-    function frontEndOneTwo() {
-      TypewriterAnimation(
-        "#frontEndOneTwo",
-        2_000,
-        100,
-        1000,
-        frontEndTwoCamera
-      );
-    }
-    // frontEndTwoCamera();
-    function frontEndTwoCamera() {
-      Camera.frontEndTwoView(frontEndTwoOne);
-    }
-    function frontEndTwoOne() {
-      TypewriterAnimation("#frontEndTwoOne", 2_000, 100, 1000, frontEndTwoTwo);
-    }
-    function frontEndTwoTwo() {
-      TypewriterAnimation("#frontEndTwoTwo", 2_000, 100, 1000, test);
-    }
-
     function test(e) {
-      console.log(e);
+      console.log("done");
     }
   }
   return { HouseScene, GarageScene };
