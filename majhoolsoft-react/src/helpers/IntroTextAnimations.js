772cbc5ddea6dded05ae9bd1d411c4b530a02838
Yousef <yousef.fatouraa@aut.ac.ir>
2022-02-25 13:44:49 +0330

    optimization

--- majhoolsoft-react/src/helpers/IntroTextAnimations.js
+++ majhoolsoft-react/src/helpers/IntroTextAnimations.js
@@ -1,35 +1,45 @@
 import anime from "animejs";
 function IntroTextAnimations() {
-  anime
-    .timeline({ loop: false })
-    // Hi text fades in
-    .add({
-      targets: ".hi",
-      opacity: [0, 1],
-      easing: "easeInExpo",
-    })
-    // welcome text fades in
-    .add({
-      targets: ".welcome",
-      opacity: [0, 1],
-      easing: "easeInExpo",
-    })
-    // Hi and welcome text fade out
-    .add(
-      {
-        targets: [".hi", ".welcome"],
-        opacity: 0,
+  function introScene() {
+    anime
+      .timeline({ loop: false })
+      // Hi text fades in
+      .add({
+        targets: ".hi",
+        opacity: [0, 1],
         easing: "easeInExpo",
-      },
-      5000
-    )
-    .add(
-      {
-        targets: ".scroll",
-        opacity: [0, 0.8],
+      })
+      // welcome text fades in
+      .add({
+        targets: ".welcome",
+        opacity: [0, 1],
         easing: "easeInExpo",
-      },
-      15000
-    );
+      })
+      // Hi and welcome text fade out
+      .add(
+        {
+          targets: [".hi", ".welcome"],
+          opacity: 0,
+          easing: "easeInExpo",
+        },
+        5000
+      )
+      .add(
+        {
+          targets: ".scroll",
+          opacity: [0, 0.8],
+          easing: "easeInExpo",
+        },
+        15000
+      );
+  }
+  function removeScrollIcon() {
+    anime.remove([".scrollIcon", "#darkness", ".scroll"]);
+    anime({
+      targets: [".scrollIcon", "#darkness", ".scroll"],
+      opacity: 0,
+    });
+  }
+  return { introScene, removeScrollIcon };
 }
 export default IntroTextAnimations;
