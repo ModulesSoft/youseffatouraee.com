0d2c6273c3a5b6edbc5b6350dc0a2ae2f481b298
Yousef Fatouraee <fatouraee96@gmail.com>
2022-03-08 10:59:34 +0330

    car bounce animation

--- majhoolsoft-react/src/helpers/CarAnimations.js
+++ majhoolsoft-react/src/helpers/CarAnimations.js
@@ -2,8 +2,15 @@ import anime from "animejs";
 function CarAnimations(finishedCallback) {
   anime({
     targets: "#rear-wheel , #front-wheel",
-    rotate: "21turn",
-    duration: 11000,
+    rotate: [{ value: "21turn", duration: 11000 }],
+    translateY: [
+      { value: 1, easing: "easeInSine", duration: 100, delay: 1000 },
+      { value: -1, easing: "easeInSine", duration: 100 },
+      { value: 0, easing: "easeInSine", duration: 300 },
+      { value: -1, easing: "easeInSine", duration: 100, delay: 1000 },
+      { value: 1, easing: "easeInSine", duration: 100 },
+      { value: 0, easing: "easeInSine", duration: 300 },
+    ],
     // easing: "cubicBezier(1,0,.45,.45)",
     easing: "easeOutQuad",
   });
@@ -12,6 +19,12 @@ function CarAnimations(finishedCallback) {
     translateX: 1050,
     duration: 6000,
     delay: 5000,
+    translateY: [
+      { value: -2, easing: "easeInSine", duration: 200, delay: 1200 },
+      { value: 2, easing: "easeInSine", duration: 200 },
+      { value: -2, easing: "easeInSine", duration: 200, delay: 1200 },
+      { value: 2, easing: "easeInSine", duration: 200 },
+    ],
     // easing: "cubicBezier(1,0,.45,.45)",
     easing: "easeOutQuad",
   }).finished.then(finishedCallback);
