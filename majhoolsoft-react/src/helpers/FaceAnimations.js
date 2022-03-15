eed0673e78e52f0ffae78e3fe48c8f27b1bbba6f
Yousef Fatouraee <fatouraee96@gmail.com>
2022-03-15 20:39:05 +0330

    face animations refactored and done

--- majhoolsoft-react/src/helpers/FaceAnimations.js
+++ majhoolsoft-react/src/helpers/FaceAnimations.js
@@ -1,44 +1,49 @@
 import anime from "animejs";
-function FaceAnimations(isMobile, mobileTarget, desktopTarget, faceUp = false) {
-  if (isMobile) {
-    anime({
-      targets: mobileTarget,
-      duration: 5000,
-      easing: "easeOutQuad",
-      y: 45,
-    });
-  } else {
-    if (faceUp) {
-      anime
-        .timeline({ loop: false })
-        .add({
-          targets: desktopTarget,
-          opacity: 0,
-        })
-        .add({
-          targets: mobileTarget,
-          duration: 2000,
-          easing: "easeOutQuad",
-          y: 45,
-        });
-    } else {
-      anime
-        .timeline({ loop: false })
-        .add({
-          targets: mobileTarget,
-          duration: 2000,
-          easing: "easeOutQuad",
-          y: 60,
-        })
-        .add({
-          targets: desktopTarget,
-          duration: 2000,
-          easing: "easeOutQuad",
-          translateX: 20,
-          translateY: -6,
-          rotate: "50deg",
-        });
-    }
+function FaceAnimations(pokerFace, smileFace) {
+  function poker() {
+    anime
+      .timeline({ loop: false })
+      .add({
+        targets: smileFace,
+        opacity: 0,
+      })
+      .add({
+        targets: pokerFace,
+        opacity: 1,
+      })
+      .add({
+        targets: pokerFace,
+        duration: 2000,
+        easing: "easeOutQuad",
+        y: 45,
+      });
   }
+  function smile() {
+    anime
+      .timeline({ loop: false })
+      .add({
+        targets: pokerFace,
+        opacity: 0,
+      })
+      .add({
+        targets: smileFace,
+        opacity: 1,
+      })
+      .add({
+        targets: pokerFace,
+        duration: 2000,
+        easing: "easeOutQuad",
+        y: 60,
+      })
+      .add({
+        targets: smileFace,
+        duration: 2000,
+        easing: "easeOutQuad",
+        translateX: 20,
+        translateY: -6,
+        rotate: "50deg",
+      });
+  }
+  return { poker, smile };
 }
 export default FaceAnimations;
