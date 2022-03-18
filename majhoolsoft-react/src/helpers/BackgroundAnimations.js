b9e32dc68792fe045060b1b8de3069b195997418
Yousef <yousef.fatouraa@aut.ac.ir>
2022-03-19 01:08:12 +0330

    day and night fixed

--- majhoolsoft-react/src/helpers/BackgroundAnimations.js
+++ majhoolsoft-react/src/helpers/BackgroundAnimations.js
@@ -1,57 +1,69 @@
 import anime from "animejs";
 function BackgroundAnimations() {
-  anime
-    .timeline({ loop: false })
-    // sun sets
-    .add(
-      {
-        targets: "#sun",
-        duration: 3000,
-        translateY: "-650px",
-        easing: "easeOutQuad",
-      },
-      1000 //delay to start
-    )
-    // clouds come up
-    .add({
-      targets: "#clouds",
-      duration: 1000,
-      translateY: "-300px",
-    });
-  // fade in to day
-  anime
-    .timeline({ loop: false })
-    .add({
-      targets: "#darkness",
-      opacity: [1, 0],
-      duration: 2000,
-      delay: 7000,
-      easing: "linear",
-    })
-    .add(
-      {
-        targets: "#background",
-        opacity: [0, 1],
+  function day() {
+    anime
+      .timeline({ loop: false })
+      // sun sets
+      .add(
+        {
+          targets: "#sun",
+          duration: 3000,
+          translateY: "-650px",
+          easing: "easeOutQuad",
+        },
+        1000 //delay to start
+      )
+      // clouds come up
+      .add({
+        targets: "#clouds",
         duration: 1000,
-        easing: "linear",
-      },
-      "-=3000"
-    )
-    .add({
-      targets: ".page",
-      backgroundColor: "#87CEEB",
-      duration: 2000,
-      easing: "linear",
-    });
+        translateY: "-300px",
+      })
+      .add(
+        {
+          targets: "#darkness",
+          opacity: 0,
+          duration: 2000,
+          easing: "linear",
+        },
+        "-=3000"
+      )
 
-  //thread four (headlights):
-  // turn off the head lights
-  anime({
-    targets: "#beam",
-    duration: 500,
-    opacity: 0,
-    easing: "easeOutQuad",
-    delay: 10000,
-  });
+      .add(
+        {
+          targets: ".page",
+          backgroundColor: "#87CEEB",
+          duration: 2000,
+          easing: "linear",
+        },
+        "-=4000"
+      );
+    // turn off the head lights
+    anime({
+      targets: "#beam",
+      duration: 500,
+      opacity: 0,
+      easing: "easeOutQuad",
+      delay: 10000,
+    });
+    // to prevent appearing again
+    anime({
+      targets: "#darkness",
+      translateY: 1080,
+      duration: 500,
+      easing: "linear",
+      delay: 6000,
+    });
+  }
+  function night() {
+    anime({
+      targets: "#darkness",
+      duration: 0,
+      opacity: 1,
+      easing: "easeOutQuad",
+      delay: 0,
+    });
+  }
+  return { night, day };
 }
 export default BackgroundAnimations;
