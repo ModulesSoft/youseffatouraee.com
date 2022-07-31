5a7fe219ff9ccf9eb5fc3ea22a56fac1983c71ce
Yousef Fatouraee <fatouraee96@gmail.com>
2022-07-31 14:10:08 +0430

    removed extra lines

--- src/animations/DoorAnimations.js
+++ src/animations/DoorAnimations.js
@@ -1,11 +1,9 @@
 import anime from "animejs";
 function DoorAnimations(doorId, amount) {
-  // turn off the head lights
   anime({
     targets: doorId,
     duration: 0,
-    opacity: 0,
-    easing: "easeOutQuad",
     translateY: amount,
   });
 }
+export default DoorAnimations;
