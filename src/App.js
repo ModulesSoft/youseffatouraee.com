5f0a318051678456fd02ac4ee6d39ca82150c197
majhoolsoft <yousef.fatouraee@aut.ac.ir>
2022-08-01 13:10:32 +0430

    extracted images and debugged laggy images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import "./App.css";
 import useWindowDimensions from "./components/GetWindowDimensions";
-import Car from "./components/Car";
-import Sideview from "./components/faces/Sideview";
 import Background from "./components/Background";
-import Darkness from "./components/Darkness";
-import LightBeam from "./components/LightBeam";
-import GarageDoor from "./components/GarageDoor";
-import Garage from "./components/Garage";
 import GetTextArray from "./components/GetTextArray";
 import Render from "./Render";
 import { useRef } from "react";
@@ -29,12 +23,6 @@ function App() {
           ref={pageRef}
         >
           <Background />
-          <Garage />
-          <GarageDoor />
-          <Sideview />
-          <Car />
-          <Darkness />
-          <LightBeam />
         </svg>
       </div>
     </div>
