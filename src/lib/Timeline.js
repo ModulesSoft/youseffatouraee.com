1fea4804e7a06abc3da6c1ea950f1d576d4fa6ca
Yousef Fatouraee <fatouraee96@gmail.com>
2023-12-12 17:42:33 -0700

    Add timeline conception

---
+++ src/lib/Timeline.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+function Timeline({ time, direction, screen }) {
+  /*
+    Camera.view(doorView)
+    Animation.scrollInstruction(-1)
+
+    Camera.zoom(doorView,personView,5s)
+    Animation.person(3s)
+    Typewriter.write(intro)
+    
+    Camera.zoom(personView,doorView,2)
+    Typewriter.write(certificatesText[0])
+
+    Camera.zoom(doorView,certificatesView[0],3s)
+    Typewriter.write(certificateText[1])
+    Camera.move(certificatesView[0],certificatesView[1])
+    Typewriter.write(certificateText[2])
+
+    Camera.zoom(certificatesView[1],libraryView,3s)
+    Typewriter.write(skillsText[0])
+    
+    Camera.zoom(libraryView,skillsView[0],3s)
+    Typewriter.write(skillsText[1])
+    
+    Camera.move(booksBegin,skillsView[1],3s)
+    Typewriter.write(skillsText[2])
+
+    Camera.zoom(booksEndView,libraryView)
+    Typewriter.write(hobbiesText[0])
+
+    Camera.zoom(libraryView,hobbiesView[0])
+    Typewriter.write(hobbiesText[1])
+
+    Camera.move(hobbiesView[0],hobbiesView[1])
+    Typewriter.write(hobbiesText[2])
+
+    Camera.zoom(hobbiesView[1],generalView)
+    Animation.door()
+
+    Animation.walk()
+    Animation.drive()
+
+    Camera.zoom(generalView,treesView)
+    Typewriter.write(hobbiesText[3])
+
+    Camera.zoom(treesView,generalView)
+    Camera.zoom(generalView,mountainView)
+    Animation.flag()
+    Typewriter.write(hobbiesText[4])
+    Animation.scrollup()
+    */
+  }
+  return <div>Timeline</div>;
+}
+
+export default Timeline;
