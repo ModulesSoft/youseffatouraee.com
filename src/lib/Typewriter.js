4ad89318d97d13bb2ee79dce85365477c9256de1
Yousef Fatouraee <fatouraee96@gmail.com>
2023-12-13 12:30:48 -0700

    Change Typewriter structure to a class

--- src/lib/Typewriter.js
+++ src/lib/Typewriter.js
@@ -1,28 +1,36 @@
 import TW from "typewriter-effect/dist/core";
-let prevText = "";
-function Typewriter(
-  pageClass,
-  wrapperClassName,
-  delay,
-  textArray,
-  position = null
-) {
-  if (!position || prevText === textArray) {
-    return;
+export default class Typewriter {
+  constructor(pageClass, wrapperClassName, delay, deleteSpeed, pauseFor) {
+    this.text = "";
+    this.pageClass = pageClass;
+    this.wrapperClassName = wrapperClassName;
+    this.delay = delay;
+    this.deleteSpeed = deleteSpeed;
+    this.pauseFor = pauseFor;
   }
-  prevText = textArray;
-  const tw = new TW(pageClass, {
-    strings: textArray,
-    autoStart: true,
-    delay: delay,
-    wrapperClassName: wrapperClassName,
-    pauseFor: delay + 3000,
-    deleteSpeed: 50,
-  });
-  return tw;
+  write(textArray) {
+    // Check for duplication
+    if (this.text === textArray) {
+      return;
+    }
+    // Copy the new text to the last text
+    this.text = textArray;
+    // Render the typewriter
+    this.typewriter = new TW(this.pageClass, {
+      strings: textArray,
+      autoStart: true,
+      delay: this.delay,
+      wrapperClassName: this.wrapperClassName,
+      pauseFor: this.pauseFor,
+      deleteSpeed: this.deleteSpeed,
+    });
+  }
+  stop() {
+    // Stop writing text
+    this.typewriter.stop();
+  }
+  clear() {
+    // Clear the written text
+    this.typewriter.deleteAll();
+  }
 }
-const CleanTypewriter = (pageClass) => {
-  const tw = new TW(pageClass).stop();
-  return tw;
-};
-export { Typewriter, CleanTypewriter };
