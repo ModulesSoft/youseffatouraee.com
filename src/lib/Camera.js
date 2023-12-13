77d48c532afd3ba48b498c15690f5ce40757b5d3
Yousef Fatouraee <fatouraee96@gmail.com>
2023-12-13 11:29:23 -0700

    Add zoom method

--- src/lib/Camera.js
+++ src/lib/Camera.js
@@ -5,5 +5,25 @@ class Camera {
   show(view) {
     this.screen.current?.setAttribute("viewBox", view);
   }
+  zoom(from, to, level, maxLevel) {
+    // Parse viewport values from strings
+    const fromViewport = from.split(" ").map(parseFloat);
+    const toViewport = to.split(" ").map(parseFloat);
+
+    // Calculate the intermediate viewport based on the zoom level
+    const intermediateViewport = fromViewport.map((value, index) => {
+      const delta = (toViewport[index] - value) * (level / maxLevel);
+      const result = value + delta;
+      return Math.max(result, 0); // Ensure non-negative values
+    });
+
+    // Construct the intermediate viewport string
+    const intermediateViewportString = intermediateViewport.join(" ");
+
+    // Update viewBox
+    this.screen.current?.setAttribute("viewBox", intermediateViewportString);
+  }
+
+  move(from, to, steps) {}
 }
 export default Camera;
