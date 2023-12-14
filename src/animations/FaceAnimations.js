65072c2767a3b11ad04b9ade8ab254e9fa9b8f49
Yousef Fatouraee <fatouraee96@gmail.com>
2023-12-13 18:40:59 -0700

    Remove the external library and add pure JS/CSS animation

--- src/animations/FaceAnimations.js
+++ src/animations/FaceAnimations.js
@@ -1,17 +1,8 @@
-import anime from "animejs";
-export function poker(pokerFaceId, scroll) {
-  anime({
-    targets: pokerFaceId,
-    easing: "easeOutQuad",
-    translateY: -1 * scroll,
-  }).finished.then(() => anime.remove(pokerFaceId));
+export function poker(pokerFaceId, step) {
+  document.querySelector(pokerFaceId).style.transform = `translateY(${step}px)`;
 }
-export function smile(smileFaceId, scroll) {
-  anime({
-    targets: smileFaceId,
-    easing: "easeOutQuad",
-    translateX: 2 * scroll,
-    translateY: (-1 / 2) * scroll,
-    rotate: 5 * scroll + "deg",
-  }).finished.then(() => anime.remove(smileFaceId));
+export function smile(smileFaceId, step) {
+  document.querySelector(smileFaceId).style.transform = `translate(${
+    2 * step
+  }px, ${(-1 / 2) * step}px) rotate(${5 * step}deg)`;
 }
