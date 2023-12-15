72237fdb94ba9410d605a8e18b40392e804aa24d
Yousef Fatouraee <fatouraee96@gmail.com>
2023-12-14 19:24:08 -0700

    Remove the external library and add pure JS/CSS animation

--- src/animations/BackgroundAnimations.js
+++ src/animations/BackgroundAnimations.js
@@ -1,53 +1,23 @@
-import anime from "animejs";
-export function day(pageClass, darknessId, cloudsId, sunId, scroll) {
-  let intScroll = Number.parseInt(scroll);
-  let sunPosition = -65 * intScroll + "px";
-  let cloudsPosition = -30 * intScroll + "px";
-  let darknessOpacity = 1 - intScroll / 10;
-  let redAmount = 13.5 * intScroll;
-  let greenAmount = 20.6 * intScroll;
-  let blueAmount = 23.8 * intScroll;
-
+export function day(pageClass, cloudsId, sunId, step, maxStep) {
+  const slideStep = step / maxStep;
+  const sunPosition = -650 * slideStep;
+  const cloudsPosition = -300 * slideStep;
+  const red = 135 * slideStep;
+  const green = 206 * slideStep;
+  const blue = 238 * slideStep;
   // sun sets
-  anime({
-    targets: sunId,
-    translateY: sunPosition,
-    easing: "easeOutQuad",
-  });
+  document.querySelector(
+    sunId
+  ).style.transform = `translateY(${sunPosition}px)`;
   // clouds come up
-  anime({
-    targets: cloudsId,
-    duration: 1000,
-    translateY: cloudsPosition,
-    delay: 1000,
-  });
-  // darkness fades away
-  anime({
-    targets: darknessId,
-    opacity: darknessOpacity,
-  });
+  document.querySelector(
+    cloudsId
+  ).style.transform = `translateY(${cloudsPosition}px)`;
   // sky color changes - desired blue: #87CEEE
-  anime({
-    targets: pageClass,
-    backgroundColor: `rgb(${redAmount}, ${greenAmount}, ${blueAmount})`,
-    easing: "linear",
-  });
-  if (darknessOpacity < 0.1) {
-    if (document.querySelector(darknessId)) {
-      document.querySelector(darknessId).style.visibility = "hidden";
-    }
-  } else {
-    night(darknessId);
-  }
+  document.querySelector(
+    pageClass
+  ).style = `background-color:rgb(${red}, ${green}, ${blue})`;
 }
-export function night(darknessId) {
-  if (document.querySelector(darknessId))
-    document.querySelector(darknessId).style.visibility = "";
-  // anime({
-  //   targets: darknessId,
-  //   duration: 0,
-  //   opacity: 1,
-  //   easing: "easeOutQuad",
-  //   delay: 0,
-  // }).finished.then(() => anime.remove(darknessId));
+export function night(pageId, step, maxStep) {
+  document.querySelector(pageId).style.filter = `brightness(${step / maxStep})`;
 }
