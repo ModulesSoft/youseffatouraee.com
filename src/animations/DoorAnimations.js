e2d7e18c00b842eac7935c857ecfa44a478b0091
Yousef Fatouraee <fatouraee96@gmail.com>
2023-12-14 18:36:11 -0700

    Replace anime.js with raw JS and modify steps

--- src/animations/DoorAnimations.js
+++ src/animations/DoorAnimations.js
@@ -1,11 +1,6 @@
-import anime from "animejs";
-function DoorAnimations(doorId, scroll) {
-  let slideAmount = Math.ceil(scroll) * 14 + "px";
-  if (scroll < 1) slideAmount = 0;
-  anime({
-    targets: doorId,
-    duration: 0,
-    translateY: slideAmount,
-  });
+function DoorAnimations(doorId, step, maxStep) {
+  let slideStep = (step / maxStep) * 150;
+  if (step < 1) slideStep = 0;
+  document.querySelector(doorId).style.transform = `translateY(${slideStep}px)`;
 }
 export default DoorAnimations;
