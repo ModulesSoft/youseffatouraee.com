856781a1d8e5471154d204d91eb4f64865813727
Yousef Fatouraee <fatouraee96@gmail.com>
2023-12-17 18:12:54 -0700

    Remove the external library and add pure JS/CSS animation according to steps

--- src/animations/CarAnimations.js
+++ src/animations/CarAnimations.js
@@ -1,56 +1,21 @@
-import anime from "animejs";
 function CarAnimations(
   carId,
   beamId,
   rearWheelId,
   frontWheelId,
-  wheelsId,
-  sideviewId
+  faceId,
+  step,
+  maxStep
 ) {
-  // turn off the head lights
-  anime({
-    targets: beamId,
-    duration: 0,
-    opacity: 0,
-    easing: "easeOutQuad",
-  });
-  anime({
-    targets: [rearWheelId, frontWheelId],
-    rotate: [{ value: "21turn", duration: 11000 }],
-    easing: "easeOutQuad",
-  });
-  anime({
-    targets: wheelsId,
-    translateY: [
-      { value: -5, easing: "linear", duration: 200, delay: 1000 },
-      { value: 0, easing: "easeOutBounce", duration: 1000 },
-      { value: -7, easing: "linear", duration: 200 },
-      { value: 0, easing: "easeOutBounce" },
-    ],
-    easing: "easeOutQuad",
-  });
-  anime({
-    targets: `${carId},${beamId},${sideviewId}`,
-    translateX: 1050,
-    duration: 6000,
-    // delay: 5000,
-    translateY: [
-      { value: -1, easing: "linear", duration: 200, delay: 1100 },
-      { value: 0, easing: "easeOutBounce", duration: 900 },
-      { value: -3, easing: "linear", duration: 200 },
-      { value: 0, easing: "easeOutBounce" },
-    ],
-    easing: "easeOutQuad",
-  }).finished.then(() => {
-    anime.remove(
-      `${carId},${beamId},${rearWheelId},${frontWheelId},${wheelsId},${sideviewId}`
-    );
-    if (document.querySelector(carId)) {
-      document.querySelector(carId).style.visibility = "hidden";
-      document.querySelector(beamId).style.visibility = "hidden";
-      document.querySelector(sideviewId).style.visibility = "hidden";
-    }
-  });
+  // Rotate the wheels
+  const angle = (step / maxStep) * 360;
+  document.querySelector(rearWheelId).style.transform = `rotate(${angle}deg)`;
+  document.querySelector(frontWheelId).style.transform = `rotate(${angle}deg)`;
+  // Move the car
+  const distance = (step / maxStep) * 600;
+  document.querySelector(carId).style.transform = `translateX(${distance}px)`;
+  document.querySelector(beamId).style.transform = `translateX(${distance}px)`;
+  document.querySelector(faceId).style.transform = `translateX(${distance}px)`;
 }
 
 export default CarAnimations;
