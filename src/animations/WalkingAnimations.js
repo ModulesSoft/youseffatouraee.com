09d9321d12ecc0f9b8a2c4ee74294af7770fad0e
Yousef Fatouraee <fatouraee96@gmail.com>
2023-12-18 17:10:47 -0700

    Remove extra lines related to the side view

--- src/animations/WalkingAnimations.js
+++ src/animations/WalkingAnimations.js
@@ -1,22 +1,16 @@
-function WalkingAnimations(walkingId, cycleIds, sideviewId, step, maxStep) {
-  const roundedStep = Math.floor((step / maxStep) * 9); // 9 to make sure it exceeds 8 and enters else condition
+function WalkingAnimations(walkingId, cycleIds, step, maxStep) {
+  const roundedStep = Math.floor((step / maxStep) * (cycleIds.length + 1)); // +1 to make sure it exceeds walking cycles and stops rendering it
   // Hide the other cycles
   cycleIds.forEach((cycleId) => {
     document.querySelector(cycleId).style.opacity = 0;
   });
-  if (roundedStep >= 0 && roundedStep < 8) {
-    console.log(roundedStep);
+  if (roundedStep >= 0 && roundedStep < cycleIds.length) {
     // Show the current cycle
     document.querySelector(cycleIds[roundedStep]).style.opacity = 1;
     // Move the body
     document.querySelector(walkingId).style.transform = `translateX(${
       roundedStep * 60
     }px)`;
-  } else {
-    // Show the face in the car
-    document.querySelector(sideviewId).style.opacity = 1;
-    // Never show the body again
-    document.querySelector(walkingId).style.opacity = 0;
   }
 }
 export default WalkingAnimations;
