6a92c6d0ce565bfc4055606f5523c0b2e27e3ae1
Yousef Fatouraee <fatouraee96@gmail.com>
2023-12-18 17:44:25 -0700

    Change car image and its animation and remove beam and face

--- src/animations/CarAnimations.js
+++ src/animations/CarAnimations.js
@@ -1,21 +1,12 @@
-function CarAnimations(
-  carId,
-  beamId,
-  rearWheelId,
-  frontWheelId,
-  faceId,
-  step,
-  maxStep
-) {
+function CarAnimations(carId, rearWheelId, frontWheelId, step, maxStep) {
   // Rotate the wheels
   const angle = (step / maxStep) * 360;
   document.querySelector(rearWheelId).style.transform = `rotate(${angle}deg)`;
   document.querySelector(frontWheelId).style.transform = `rotate(${angle}deg)`;
   // Move the car
-  const distance = (step / maxStep) * 600;
+  const distance = (step / maxStep) * 3000;
   document.querySelector(carId).style.transform = `translateX(${distance}px)`;
   document.querySelector(beamId).style.transform = `translateX(${distance}px)`;
-  document.querySelector(faceId).style.transform = `translateX(${distance}px)`;
 }
 
 export default CarAnimations;
