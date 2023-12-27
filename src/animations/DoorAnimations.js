b4301c925761f12d8b1e96ae400cd6a2e11c5a41
Yousef Fatouraee <fatouraee96@gmail.com>
2023-12-27 16:54:38 -0700

    Tweak sliding steps

--- src/animations/DoorAnimations.js
+++ src/animations/DoorAnimations.js
@@ -1,5 +1,5 @@
 function DoorAnimations(doorId, step, maxStep) {
-  let slideStep = (step / maxStep) * 150;
+  let slideStep = (step / maxStep) * 140;
   if (step < 1) slideStep = 0;
   document.querySelector(doorId).style.transform = `translateY(${slideStep}px)`;
 }
