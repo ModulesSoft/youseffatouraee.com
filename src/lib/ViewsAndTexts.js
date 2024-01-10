8029b7c29ebb7c58d4c124590b82c31d3a2d1236
Yousef Fatouraee <fatouraee96@gmail.com>
2024-01-10 12:05:20 -0700

    Tweak views

--- src/lib/ViewsAndTexts.js
+++ src/lib/ViewsAndTexts.js
@@ -1,20 +1,7 @@
 const ViewsAndTexts = (isMobile, width, height, texts) => {
   let viewsAndTexts = {
-    general: {
-      view: isMobile
-        ? `600 ${1080 - height} ${width} ${height}`
-        : "0 0 1920 1080",
-    },
-    walking: {
-      view: isMobile ? "1300 0 620 1080" : "300 0 1620 1080",
-    },
-    door: {
-      view: isMobile
-        ? `262 ${1080 - height} ${width} ${height}`
-        : "262 800 400 240",
-    },
     laptop: {
-      view: "600 920 60 60",
+      view: isMobile ? "600 950 60 60" : "600 920 60 60",
     },
     bachelor: {
       view: isMobile ? "550 890 60 45" : "550 890 60 35",
@@ -28,11 +15,28 @@ const ViewsAndTexts = (isMobile, width, height, texts) => {
     garageHobbies: {
       view: isMobile ? "272 850 390 220" : "262 850 200 180",
     },
+    door: {
+      view: isMobile
+        ? `262 ${1080 - height} ${width} ${height}`
+        : "262 800 400 240",
+    },
+    general: {
+      view: isMobile
+        ? `600 ${1080 - height} ${width - 50} ${height - 50}`
+        : "0 0 1920 1080",
+    },
+    walking: {
+      view: isMobile
+        ? `1510 ${1080 - height} ${width} ${height}`
+        : "300 0 1620 1080",
+    },
     woods: {
-      view: isMobile ? "1100 200 1200 840" : "700 200 1200 840",
+      view: isMobile
+        ? `1350 ${1080 - height} ${width - 50} ${height}`
+        : "700 200 1200 840",
     },
     mountain: {
-      view: "970 100 600 420",
+      view: isMobile ? `970 0 ${width} ${height}` : "970 100 600 420",
     },
   };
   // add texts to viewsAndTexts
