3e194e62048ebe864d82a744a6657c656e885874
Yousef Fatouraee <fatouraee96@gmail.com>
2024-02-03 22:21:27 -0700

    Clean up code and extract const values

--- src/animations/FlagAnimations.js
+++ src/animations/FlagAnimations.js
@@ -1,6 +1,7 @@
 function FlagAnimations(flagId, step, maxStep) {
+  const distance = -18;
   document.querySelector(flagId).style.transform = `translateY(${
-    (step / maxStep) * -18
+    (step / maxStep) * distance
   }px)`;
 }
 export default FlagAnimations;
