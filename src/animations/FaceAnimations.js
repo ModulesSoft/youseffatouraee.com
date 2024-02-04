d99e1cf2f87d9736d94704d7827acbaa433d8fc6
Yousef Fatouraee <fatouraee96@gmail.com>
2024-02-03 22:21:31 -0700

    Clean up code and extract const values

--- src/animations/FaceAnimations.js
+++ src/animations/FaceAnimations.js
@@ -1,10 +1,12 @@
 export function poker(pokerFaceId, step, maxStep) {
+  const distance = 30;
   document.querySelector(pokerFaceId).style.transform = `translateY(${
-    (step / maxStep) * 30
+    (step / maxStep) * distance
   }px)`;
 }
 export function smile(smileFaceId, step, maxStep) {
+  const distance = 20;
   document.querySelector(smileFaceId).style.transform = `translate(${
-    (step / maxStep) * 20
+    (step / maxStep) * distance
   }px, ${(step / maxStep) * -4}px) rotate(${(step / maxStep) * 45}deg)`;
 }
