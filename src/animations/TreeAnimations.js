f48584abc727492b0063e55aa1e7a09d95469b5e
Yousef Fatouraee <fatouraee96@gmail.com>
2024-02-03 22:20:48 -0700

    Clean up code and extract const values

--- src/animations/TreeAnimations.js
+++ src/animations/TreeAnimations.js
@@ -1,9 +1,11 @@
 function TreeAnimations(treeOneId, treeTwoId, step, maxStep) {
+  const distanceOne = 20;
+  const distanceTwo = -20;
   document.querySelector(treeOneId).style.transform = `translateX(${
-    (step / maxStep) * 20
+    (step / maxStep) * distanceOne
   }px)`;
   document.querySelector(treeTwoId).style.transform = `translateX(${
-    (step / maxStep) * -20
+    (step / maxStep) * distanceTwo
   }px)`;
 }
 export default TreeAnimations;
